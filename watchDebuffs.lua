ff95b8484177e9a37d89fd3ebf0580e9ee5a8a2a
Matt <matt@mdev.me>
2014-01-10 20:02:33 +0000

    Added root,silence,stun and (test)friendly to Debuffs.types
    Added more funcs for dynamic looping through Debuffs.types to get warnings
    
    Made Debuffs.get() pass debuffs to the appropriate object in Debuffs.types

--- watchDebuffs.lua
+++ watchDebuffs.lua
@@ -15,6 +15,20 @@ Debuffs = {
 			,message = 'is incapacitated for [remaining] seconds ({SPELL_LINK})'
 			,recoverMessage = 'is no longer incapacitated'
 		}
+		,root = {
+			debuffs = {}
+			,names = {'Freeze','Root','Entangling Roots','Frozen'}
+			,descTerms = {' [rR]oot','^Rooted','[fF]rozen'}
+			,message = 'has been rooted for [remaining] seconds ({SPELL_LINK})'
+			,recoverMessage = 'is no longer rooted'
+		}
+		,silence = {
+			debuffs = {}
+			,names = {'Silence','Solar Beam','Strangulate','Arcane Torrent','Silencing Shot'}
+			,descTerms = {' [sS]ilenced?','^Silenced'}
+			,message = 'has been silenced for [remaining] seconds ({SPELL_LINK})'
+			,recoverMessage = 'is no longer silenced'
+		}
 		,slow = {
 			debuffs = {}
 			,names = {'Dazed','Daze','Slow','Slowed','Hamstring','Ice Trap'}
@@ -31,7 +45,69 @@ Debuffs = {
 			end
 			,recoverMessage = 'is no longer slowed'
 		}
+		,stun = {
+			debuffs = {}
+			,names = {'Stun','Stunned','Charge','Stomp'}
+			,descTerms = {' [sS]tun','^Stun','[iI]mmobili[sz]ed'}
+			,message = 'has been stunned for [remaining] seconds ({SPELL_LINK})'
+			,recoverMessage = 'is no longer stunned'
+		}
+		,friendly = {
+			debuffs = {}
+			,names = {'Mark of the Wild','Blessing of Kings'}
+			,descTerms = {'increased by'}
+			,message = 'is buffed by {SPELL_LINK}'
+			,recoverMessage = 'has lost his buff'
+		}
 	}
+	,emptyTypeCache = function()
+		for type,info in pairs(Debuffs.types) do
+			Debuffs.types[type].debuffs = {}
+		end
+	end
+	,isType = function(debuff,type)
+		if(Debuffs.types[type]==nil) then return false; end
+		for k,v in pairs(Debuffs.types[type].names) do
+			if(debuff.name == v) then return true; end
+		end
+		for k,v in pairs(Debuffs.types[type].descTerms) do
+			if(string.find(debuff.desc,v)~=nil) then return true; end
+		end
+		return false;
+	end
+	,getType = function(debuff)
+		for type in pairs(Debuffs.types) do
+			if(Debuffs.isType(debuff,type)) then return type; end
+		end
+		return false;
+	end
+	,fillMsg = function(msg,debuff)
+		local ret = msg;
+		ret = ret:gsub('%[remaining%]',tostring(debuff.remaining));
+		ret = ret:gsub('%{SPELL_LINK%}',Debuffs.getLink(debuff));
+		return ret;
+	end
+	,checkDebuffs = function()
+		for dbType,info in pairs(Debuffs.types) do
+			if(#info.debuffs >= 1) then
+				local debuff = info.debuffs[1];
+				local message = info.message;
+				local lastAnnounce = info.lastAnnounce or -10;
+				local theTime = GetTime();
+				local repeatLimit = info.repeatLimit or 5;
+				if(theTime - lastAnnounce > repeatLimit) then
+					if(type(message)=="function") then message = message(debuff); end
+					local recoverMessage = info.recoverMessage;
+					if(type(recoverMessage)=="function") then recoverMessage = recoverMessage(debuff); end
+					message = Debuffs.fillMsg(message,debuff);
+					recoverMessage = Debuffs.fillMsg(recoverMessage,debuff);
+					if(debuff.remaining>0 and debuff.remaining%2==0) then LCU.announcePlayer(message);
+					elseif(debuff.remaining==0) then LCU.announcePlayer(recoverMessage) end
+					Debuffs.types[dbType].lastAnnounce = theTime;
+				end
+			end
+		end
+	end
 	,getLink = function(debuff)
 		if(type(debuff)=="number") then
 			local name,rank = GetSpellInfo(debuff);
@@ -125,11 +201,16 @@ Debuffs = {
 	,get = function(who)
 		who = who or "player";
 		local debuffs = {}
+		Debuffs.emptyTypeCache();
+		local auraType = LCU.debugMode and 'HELPFUL' or 'HARMFUL';
 		for i=1,40 do
-			local n,rank,_,_,dbType,duration,expires,_,_,_,id = UnitDebuff(who,i)
+			local n,rank,_,_,dbType,duration,expires,_,_,_,id = UnitAura(who,i,auraType);
 			if(n ~= nil and expires ~= nil) then
-				local desc = GetSpellDescription(id) or ''
-				debuffs[#debuffs+1] = {name=n,rank=rank,["type"]=(dbType or 'null'),length=duration,remaining=LCU.round(expires-GetTime()),desc=desc,id=id,extraInfo=''}
+				local desc = GetSpellDescription(id) or '';
+				local debuff = {name=n,rank=rank,["type"]=(dbType or 'null'),length=duration,remaining=LCU.round(expires-GetTime()),desc=desc,id=id,extraInfo=''};
+				debuffs[#debuffs+1] = debuff;
+				local dbType = Debuffs.getType(debuff);
+				if(dbType~=false) then table.insert(Debuffs.types[dbType].debuffs,debuff); end
 			end
 		end
 		LCU[who]['debuffs'] = debuffs;
@@ -210,8 +291,9 @@ end
 local lastDebuffMessage = 0
 function checkDebuffs()
 	Debuffs.get()
-	--if(#debuffs > 0 and GetTime()-lastDebuffMessage > 8 and LCU.debugMode==true) then
-	if(#LCU.player.debuffs > 0 and LCU.debugMode==true) then
+	Debuffs.checkDebuffs()
+	--if(#LCU.player.debuffs > 0 and LCU.debugMode==true) then
+	if(#LCU.player.debuffs > 0 and GetTime()-lastDebuffMessage > 8 and LCU.debugMode==true) then
 		--LCMessage('In control? '..tostring(LCU.player.isInControl()),nil,10)
 		for k,debuff in pairs(LCU.player.debuffs) do
 			local debuffMsg = 'Debuff #'..tostring(k)..':'
@@ -220,8 +302,6 @@ function checkDebuffs()
 			debuffMsg = debuffMsg..' ('..debuff.type..') '
 			debuffMsg = debuffMsg..' '..tostring(debuff.remaining)..' secs remaining.'
 			LCMessage(debuffMsg,nil,LCU.round(debuff.remaining/2));
-			local controlMsg = LCU.player.isInControl() and 'In Control' or 'Not In Control'
-			LCMessage(controlMsg,nil,LCU.round(debuff.remaining/2));
 		end
 		lastDebuffMessage = GetTime()
 	end

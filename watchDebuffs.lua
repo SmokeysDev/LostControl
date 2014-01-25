30729946c0894a32ac864fa2270ddfe8392a6296
Matt <matt@mdev.me>
2014-01-25 21:19:45 +0000

    Added fallback for when checking current debuff and there isn't one

--- watchDebuffs.lua
+++ watchDebuffs.lua
@@ -131,7 +131,7 @@ Debuffs = {
 				if(dbType==false and LCU.debugMode) then LCU.sendMsg('Couldnt find type for "'..debuff.name..'" = "'..debuff.desc..'"') end
 				if(dbType~=false) then
 					local currD = Debuffs.types[dbType].debuff;
-					if(not (currD.id ~= debuff.id and debuff.remaining<LCcfg.get('minDebuffTime'))) then
+					if(type(currD)=='table' and not (currD.id ~= debuff.id and debuff.remaining<LCcfg.get('minDebuffTime'))) then
 						if(currD==false or currD.remaining < debuff.remaining or (debuff.remaining<currD.remaining and debuff.id==currD.id)) then
 							Debuffs.types[dbType].debuff = debuff;
 						end

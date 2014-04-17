cc8aadb193c9b111fe4bd3d679930a2d21317f91
Matt <matt@mdev.me>
2014-04-17 12:46:49 +0100

    Added mini how-to comments in debuffDB

--- debuffDB.lua
+++ debuffDB.lua
@@ -1,3 +1,13 @@
+-- Adding descriptions:
+-- --------------------
+--	(best method if adding multiple)	Debuffs.addDescs([debuff type],{'[string to match]','[another string to match]',....})   -> e.g. Debuffs.addDescs('silence',{'cannot cast','holy locked'})
+--	(best method if adding just one)	Debuffs.addDesc([debuff type],'[string to match]')   -> e.g. Debuffs.addDesc('slow','speed reduced')
+
+-- Adding names:
+-- --------------
+--	(best method if adding multiple)	Debuffs.addNames([debuff type],{'[debuff name]','[debuff name]',....})   -> e.g. Debuffs.addNames('slow',{'Dazed','Hamstring','Ice Trap'})
+--	(best method if adding just one)	Debuffs.addName([debuff type],'[debuff name]')   -> e.g. Debuffs.addName('stun','Stunned')
+
 Debuffs.addDescs('incap',{
 	'[uU]nable to move or act'
 	,'[cC]annot move or act'

9cac692107c44977d1e4f4e73ac1be748c3cf4e2
Matt <matt@mdev.me>
2014-11-09 20:35:19 +0000

    Changed english language to just empty the data (as the key will be used if not found)

--- lang/en.lua
+++ lang/en.lua
@@ -1,8 +1,5 @@
 LCLang.loadLang_en = function()
-	LCLang['English'] = 'English';
-	LCLang['French'] = 'French';
-	LCLang['Spanish'] = 'Spanish';
-	LCLang['is no longer feared'] = 'is no longer feared';
+	LCLang.data = {};
 end
 
 if (GetLocale() == "enUS" and GetLocale() == "enGB") then LCLang.loadLang('en') end

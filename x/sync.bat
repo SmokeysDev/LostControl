78dbcc29db2ab4551b34e48ee94177deae63f549
unknown <MattB@PC29.mansfieldsdesign.net>
2014-11-21 14:32:32 +0000

    Corrected paths on sync.bat

--- x/sync.bat
+++ x/sync.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-cd /d "E:\personal\WoW\addon_dev\LostControl\"
+cd /d "E:\personal\wow addon_dev\LostControl"
 
-robocopy "E:\personal\WoW\addon_dev\LostControl" "E:\personal\WoW\interface\addons\LostControl" *.lua *.txt *.xml /PURGE
+robocopy "E:\personal\wow addon_dev\LostControl" "E:\personal\World of Warcraft\interface\addons\LostControl" *.* /E /XD .git x /PURGE
 ::robocopy "./" "./test/" *.lua *.txt /PURGE

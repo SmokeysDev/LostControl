999fa47377923c8e710524d5a06f722be5ee1f0e
Matt Bacon <matt@mdev.me>
2018-12-16 12:05:34 +0000

    Fixed sync batch file

--- x/sync.bat
+++ x/sync.bat
@@ -2,5 +2,5 @@
 
 cd /d "C:\Users\Matt\Documents\WoW addons\LostControl"
 
-robocopy "C:\Users\Matt\Documents\WoW addons\LostControl" "G:\World of Warcraft\interface\addons\LostControl" *.* /E /PURGE /XF .gitignore *.key* /XD .git nbproject x
+robocopy "C:\Users\Matt\Documents\WoW addons\LostControl" "G:\World of Warcraft\_retail_\interface\addons\LostControl" *.* /E /PURGE /XF .gitignore *.key* /XD .git nbproject x
 ::robocopy "./" "./test/" *.lua *.txt /PURGE
